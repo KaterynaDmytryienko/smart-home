14bc0a891eb3ab14be9954548c644e54a48d6491
dmytrkat <dmytkat@cvut.cz>
2024-01-02 21:56:50 +0100

    Final commit.

--- src/test/java/PersonActivityTest.java
+++ src/test/java/PersonActivityTest.java
@@ -13,29 +13,6 @@ import static org.junit.Assert.assertTrue;
 
 public class PersonActivityTest {
     House house=House.getHouse();
-
-    @Test
-    public void personDrinkingTest(){
-        Person person=new Person("Ally","female",30,false);
-        Floor floor = new Floor(1);
-        Room room = new LivingRoom();
-        List<Person> people = new ArrayList<>();
-        List<Floor>floors = new ArrayList<>();
-        List<Room>rooms = new ArrayList<>();
-
-        floors.add(floor);
-        people.add(person);
-        house.setPeople(people);
-        house.setFloors(floors);
-        floor.setRooms(rooms);
-        Event event=new Event(Even_Types.PERSON_THIRSTY,person,room);
-        EventManager eventManager=new EventManager();
-        eventManager.handleEvent(event);
-
-        assertTrue(event.isHandled);
-
-    }
-
     @Test
     public void personWorksOutTest(){
         Person person=new Person("Ally","female",30,false);
